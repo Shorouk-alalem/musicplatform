e0ed23619fc6b18b2b46af9180a37d743f83fb5c
Shorouk Alalem <shorouk.alalem@bld.ai>
2022-10-07 15:23:57 +0000

    Upload New File

---
+++ albums/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+from artists.models import Artist 
+import datetime
+from django.utils import timezone
+# albums Model.
+class Album(models.Model):
+    artist_name=models.ForeignKey(Artist,on_delete=models.CASCADE )
+    name = models.CharField(max_length=100 ,default="New Album")
+    creation_datetime=models.DateTimeField()
+    release_datetime=models.DateTimeField(blank=False)
+    cost= models.DecimalField( blank=False, decimal_places=3 , max_digits=20)
+    def __str__(self):
+        return self.name
