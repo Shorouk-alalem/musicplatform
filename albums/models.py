e2205ef2533a54f92ad7a7bd42093efe6763eff9
Shorouk Alalem <66391713+Shorouk-alalem@users.noreply.github.com>
2022-10-21 23:53:00 +0200

    Update models.py

--- albums/models.py
+++ albums/models.py
@@ -4,10 +4,15 @@ from artists.models import Artist
 import datetime
 from django.utils import timezone
 # albums Model.
-class Album(models.Model):
+#Task3: 1- Instead of having an explicit `created_at` field in the `Album` model, inherit from [`TimeStampedModel`]
+class TimeStampedModel(models.Model):
+     creation_datetime = models.DateTimeField(auto_now_add=True)
+     class Meta:
+         abstract = True
+class Album(TimeStampedModel):
     artist_name=models.ForeignKey(Artist,on_delete=models.CASCADE )
     name = models.CharField(max_length=100 ,default="New Album")
-    creation_datetime=models.DateTimeField()
+   # creation_datetime=models.DateTimeField()
     release_datetime=models.DateTimeField(blank=False)
     cost= models.DecimalField( blank=False, decimal_places=3 , max_digits=20)
     #..1 adding is_approved field 
