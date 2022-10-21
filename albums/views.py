4dea568dffa900dc78bb30d6a3250e6fc9586f7b
Shorouk Alalem <66391713+Shorouk-alalem@users.noreply.github.com>
2022-10-21 23:54:12 +0200

    Update views.py

--- albums/views.py
+++ albums/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.shortcuts import render
+from .forms import AlbumForm
 
-# Create your views here.
+def AlbumView(request):
+    if request.method == 'POST':
+        form = AlbumForm(request.POST)
+        if form.is_valid():
+            form.save()
+    else:
+        form = AlbumForm()
+    return render(request, 'AlbumTemplate.html', {'form': form})
